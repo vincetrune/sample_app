5ab82753714e6ce73507b3dcef4eff8585232b5a
vinnie <vvtoasty@gma il.com>
2014-04-05 20:25:25 -0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
+
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
